b222930d287d28494a3a12bf2e10c0783c83f2cf
Elena <none>
2023-03-27 01:04:24 +0300

    2023/03/27 001 Ваня — большой и грозный манул. Теплый кот.

--- Deck.java
+++ Deck.java
@@ -4,20 +4,13 @@ public class Deck{
     // Объем колоды
     private final int quantity = 36;
     // Колода
-    private static Card[] cards;
+    private Card[] cards;
 
     // Создание колоды
     public Deck(){
-        int i = -1;
         cards = new Card[quantity];
         for(int kva = 0; kva < quantity; kva += 1){
             cards[kva] = new Card(kva);
-            if (kva == quantity-1){
-                i = -1;
-            } else {
-                i = kva+1;
-            }
-            cards[kva].setNext(i);
         }
     }
 
@@ -34,35 +27,27 @@ public class Deck{
     // Перемешивание колоды
     public void Shuffle(){
         Random rand = new Random();
-        int r_p, i = -1, tmp_i = -1;
+        int r_p;
         Card temp;
         for (int kva = 0; kva < quantity; kva += 1){
             r_p = rand.nextInt(36);
             temp = cards[kva];
             cards[kva] = cards[r_p];
-            tmp_i = cards[r_p].getNext();
-            if (kva == quantity - 1) i = -1;
-            if (kva < quantity - 1) i = kva+1;
-            cards[kva].setNext(i);
             cards[r_p] = temp;
-            cards[r_p].setNext(tmp_i);
         }
     }
 
     // Инкапсуляция
     public static Card[] getCards(){
-        return cards;
+        //ВЖУХ BСЁ ЗАРАБОТАЛО
+        return (new Deck()).cards;
     }
 
     // Раздача карт
     public void destribute(Player p_1, Player p_2){
-        
         for(int gav = 0; gav < 35; gav += 2){
-            cards[gav].setNext(gav);
-            cards[gav+1].setNext(gav+1);
             p_1.givecard(cards[gav]);
             p_2.givecard(cards[gav+1]);
         }
-        System.out.println();
     }
 }
--- Player.java
+++ Player.java
@@ -2,45 +2,24 @@ public class Player {
     private Card[] p_cards = Deck.getCards();
 	int index = -1;
 
-	// ТУТ БЫЛА ОШИБКА. НЕ РАБОТАЕТ. Где-то на стыке раздачи и получения нового индекса
+	// ТУТ БЫЛА ОШИБКА. НО ЕЁ БОЛЬШЕ НЕТ
     public void givecard(Card meow){
-		// if(index == -1){
-		// 	index = meow.getNext();
-		// }
-		
-		// int i = index;
-		// Card tmp;
-		// while(i != -1){
-		// 	tmp = p_cards[i];
-		// 	i = tmp.getNext();
-		// }
-		// p_cards[i].setNext(meow.getNext());
-
-		// meow.printCard();
-		// meow.printNextIndex();
-		// meow.setNext(-1);
-		int x = index;
- 		Card tmp = meow;
-
- 		if(index == -1){
- 			index = meow.getNext();
- 		}
-
-		while(x != -1){
-			tmp = p_cards[x];
-			x = tmp.getNext();
-			// tmp.printCard();
-			// отладочное
-			// tmp.printindex();
+		int i = index;
+		int o = p_cards[meow.getCard()].getCard();
+		if (i == -1){
+			index = o;
+			return;
 		}
 
-		p_cards[index] = tmp;
+		Card tmp = p_cards[i];
+		int next = tmp.getNext();
 
- 		// Отладочный вывод карты и индекса
-		// meow.printCard();
- 		// meow.printNextIndex();
- 		meow.setNext(-1);
-		
+		while (next != -1){
+			i = next;
+			tmp = p_cards[i];
+			next = tmp.getNext();
+		}
+		tmp.setNext(o);
 	}
 
 	public void test_print() {
@@ -60,6 +39,5 @@ public class Player {
 			tmp.printCard();
 			x = tmp.getNext();
 		}
-		System.out.println();
 	}
 }
